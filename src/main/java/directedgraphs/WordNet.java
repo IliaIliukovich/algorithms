af1b68a89b08ebbc6c8adbc896de853368e4f163
Ilia_Iliukovich <Ilia_Iliukovich@epam.com>
2018-01-25 20:33:31 +0300

    WordNet

--- src/main/java/directedgraphs/WordNet.java
+++ src/main/java/directedgraphs/WordNet.java
@@ -4,50 +4,64 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class WordNet {
 
-    private List<Synset> synsetList = new LinkedList<>();
-    private List<Hypernym> hypernymList = new LinkedList<>();
+    private List<Synset> synsetList;
+    private Map<String, Integer> nounsMap;
+    private Map<Integer, List<Integer>> hypernymMap;
 
     public WordNet(String synsets, String hypernyms) {
 
+        if (synsets == null || hypernyms == null) throw new IllegalArgumentException();
+
         try {
+            synsetList = new ArrayList<>();
             Files.lines(new File(synsets).toPath()).forEach(line -> {
                 synsetList.add(new Synset(line));
             });
 
+            nounsMap = new TreeMap<>();
+            synsetList.forEach(synset -> {
+                synset.sinonims.forEach(sinonim -> {
+                    nounsMap.put(sinonim, synset.id);
+                });
+            });
+
+            hypernymMap = new HashMap<>(synsetList.size());
             Files.lines(new File(hypernyms).toPath()).forEach(line -> {
-                hypernymList.add(new Hypernym(line));
+                parseLine(line, hypernymMap);
             });
         } catch (IOException e) {
             throw new IllegalArgumentException();
         }
 
-
     }
 
     public Iterable<String> nouns() {
-        return synsetList.stream().flatMap(e -> e.sinonims.stream()).collect(Collectors.toCollection(TreeSet::new));
+        return nounsMap.keySet();
     }
 
     public boolean isNoun(String word) {
-        Set set = (Set) nouns();
-        return set.contains(word);
+        if (word == null) throw new IllegalArgumentException();
+        return nounsMap.containsKey(word);
     }
 
     public int distance(String nounA, String nounB) {
-
-        Synset synsetA = synsetList.stream().filter(e -> e.sinonims.contains(nounA)).findAny().get();
-
-        return 0;
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        int[] distanceAB = new int[synsetList.size()];
+        int minDistIndex = findMinDistIndex(nounA, nounB, new boolean[synsetList.size()], new boolean[synsetList.size()], new int[synsetList.size()], distanceAB);
+        return minDistIndex == -1 ? minDistIndex : distanceAB[minDistIndex];
     }
 
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        return null;
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        int minDistIndex = findMinDistIndex(nounA, nounB, new boolean[synsetList.size()], new boolean[synsetList.size()], new int[synsetList.size()], new int[synsetList.size()]);
+        if (minDistIndex != -1) {
+            return synsetList.get(minDistIndex).print();
+        } else {
+            return null;
+        }
     }
 
     public static void main(String[] args) {
@@ -55,17 +69,59 @@ public class WordNet {
         WordNet wordNet = new WordNet("D:\\ideaProjects\\algorithms3\\src\\test-data\\directedgraphs\\wordnet\\synsets100-subgraph.txt",
                 "D:\\ideaProjects\\algorithms3\\src\\test-data\\directedgraphs\\wordnet\\hypernyms100-subgraph.txt");
 
-        wordNet.synsetList.forEach(System.out::println);
-        wordNet.hypernymList.forEach(System.out::println);
-        wordNet.nouns().forEach(System.out::println);
-        System.out.println(wordNet.isNoun("qwe"));
-        System.out.println(wordNet.isNoun("zymase"));
+        System.out.println("Distance = " + wordNet.distance("zymase", "zymase"));
+        System.out.println("Common ancestor = " + wordNet.sap("zymase", "zymase"));
+        System.out.println("Distance = " + wordNet.distance("transaminase", "transaminase"));
+        System.out.println("Common ancestor = " + wordNet.sap("transaminase", "transaminase"));
 
     }
 
+    private int findMinDistIndex(String nounA, String nounB, boolean[] isMarkedA, boolean[] isMarkedB, int[] distanceA, int[] distanceAB) {
+        depthFirstSearchMark(nounsMap.get(nounA), isMarkedA, distanceA, 0);
+        depthFirstSearchFindMinDistance(nounsMap.get(nounB), hypernymMap, isMarkedA, isMarkedB, distanceA, distanceAB, 0);
+
+        int minDistIndex = -1;
+        for (int i = 0; i < synsetList.size(); i++) {
+            if (isMarkedA[i] && isMarkedB[i]) {
+                minDistIndex = (minDistIndex != -1 && distanceAB[i] > distanceAB[minDistIndex]) ? minDistIndex : i;
+            }
+        }
+        return minDistIndex;
+    }
+
+    private void depthFirstSearchMark(Integer id, boolean[] isMarkedA, int[] distance, int curDistance) {
+        isMarkedA[id] = true;
+        distance[id] = curDistance;
+        if (hypernymMap.containsKey(id)) {
+            hypernymMap.get(id).forEach(e -> {
+                depthFirstSearchMark(e, isMarkedA, distance, curDistance + 1);
+            });
+        }
+    }
+
+    private void depthFirstSearchFindMinDistance(Integer id, Map<Integer, List<Integer>> hypernymMap, boolean[] isMarkedA, boolean[] isMarkedB, int[] distanceA, int[] distanceAB, int curDistance) {
+        isMarkedB[id] = true;
+        if (isMarkedA[id]) {
+            distanceAB[id] = distanceA[id] + curDistance;
+        }
+        if (hypernymMap.containsKey(id)) {
+            hypernymMap.get(id).forEach(e -> {
+                depthFirstSearchFindMinDistance(e, hypernymMap, isMarkedA, isMarkedB, distanceA, distanceAB, curDistance + 1);
+            });
+        }
+    }
+
+    private void parseLine(String line, Map<Integer, List<Integer>> hypernymMap) {
+        String[] split = line.split(",");
+        int id = Integer.parseInt(split[0]);
+        List<Integer> hypernymList = new LinkedList<>();
+        for (int i = 1; i < split.length; i++) {
+            hypernymList.add(Integer.parseInt(split[i]));
+        }
+        hypernymMap.put(id, hypernymList);
+    }
 
     private class Synset {
-
         int id;
         Set<String> sinonims = new TreeSet<>();
         String glos;
@@ -81,24 +137,18 @@ public class WordNet {
         public String toString() {
             return id + " " + sinonims + " " + glos;
         }
-    }
 
-    private class Hypernym {
-
-        int id;
-        List<Integer> hypernyms = new LinkedList<>();
-
-        Hypernym(String line) {
-            String[] split = line.split(",");
-            id = Integer.parseInt(split[0]);
-            for (int i = 1; i < split.length; i++) {
-                hypernyms.add(Integer.parseInt(split[i]));
+        private String print() {
+            if (sinonims.size() > 1) {
+                StringBuilder builder = new StringBuilder();
+                for (String sinonim : sinonims) {
+                    builder.append(sinonim).append(" ");
+                }
+                return builder.toString();
+            } else {
+                return sinonims.iterator().next();
             }
         }
-
-        @Override
-        public String toString() {
-            return id + " " + hypernyms;
-        }
     }
+
 }
